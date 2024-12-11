1256fc98b047209d0e3f14e475474494ba832296
mayankvagadiya2023 <mayankvagadia17@gmail.com>
2024-12-11 12:22:30 +0530

    compose

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,31 @@
+rootProject.name = "FirstDemo"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
 pluginManagement {
     repositories {
-        google()
-        gradlePluginPortal()
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
         mavenCentral()
+        gradlePluginPortal()
     }
 }
 
 dependencyResolutionManagement {
     repositories {
-        google()
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
         mavenCentral()
     }
 }
 
-rootProject.name = "first_multiplatform_demo"
-include(":androidApp")
-include(":shared")
+include(":composeApp")
